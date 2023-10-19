bfff485fcfcd7511bdbd4cf1ac739146ab8945c7
Gabriel <gabriel.assis2309@gmail.com>
2023-10-19 09:15:36 -0300

    First commit

---
+++ src/main/java/com/gabriel/drugstorea/DrugstoreAApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.drugstorea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugstoreAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugstoreAApplication.class, args);
+    }
+
+}
