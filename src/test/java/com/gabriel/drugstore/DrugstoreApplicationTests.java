938d5d412e120cc114cb440c64ebad7ed7beccca
Gabriel <gabriel.assis2309@gmail.com>
2023-10-19 10:14:40 -0300

    Main update

--- src/test/java/com/gabriel/drugstore/DrugstoreApplicationTests.java
+++ src/test/java/com/gabriel/drugstore/DrugstoreApplicationTests.java
@@ -1,10 +1,10 @@
-package com.gabriel.drugstorea;
+package com.gabriel.drugstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DrugstoreAApplicationTests {
+class DrugstoreApplicationTests {
 
     @Test
     void contextLoads() {
